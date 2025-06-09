2a8f0cd42958d3537c33026836e4d4ca74f5b80e
conao3 <conao3@gmail.com>
2025-06-10 06:14:55 +0900

    tokenize

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
 import System.IO
-import Data.Maybe (fromMaybe)
-import Data.List (intercalate)
-import Data.Char (isSpace)
+import Data.Char (isSpace, isDigit)
+
+data HaspExp = Integer Integer
+               | Symbol String
+               | List [HaspExp]
+               deriving (Eq, Show)
 
 input_ :: IO String
 input_ = do
@@ -11,18 +14,37 @@ input_ = do
     hFlush stdout
     getLine
 
-read_ :: String -> Maybe [String]
+parseValue :: String -> HaspExp
+parseValue s
+    | all isDigit s = Integer (read s)
+    | otherwise     = Symbol s
+
+tokenize :: String -> String
+tokenize [] = []
+tokenize ('(':xs) = " ( " ++ tokenize xs
+tokenize (')':xs) = " ) " ++ tokenize xs
+tokenize (x:xs) = x : tokenize xs
+
+read_ :: String -> Maybe HaspExp
 read_ s
     | all isSpace s = Nothing
-    | otherwise     = Just (words s)
+    | otherwise     = case words (tokenize s) of
+        []     -> Nothing
+        [x]    -> Just (parseValue x)
+        xs     -> Just (List (map parseValue xs))
 
-eval_ :: Maybe [String] -> Maybe String
+eval_ :: Maybe HaspExp -> Maybe HaspExp
 eval_ Nothing = Nothing
-eval_ (Just s) = Just (unwords s)
+eval_ (Just v) = Just v
 
-print_ :: Maybe String -> IO ()
+showValue :: HaspExp -> String
+showValue (Integer n) = show n
+showValue (Symbol s) = s
+showValue (List xs) = "(" ++ unwords (map showValue xs) ++ ")"
+
+print_ :: Maybe HaspExp -> IO ()
 print_ Nothing  = pure ()
-print_ (Just s) = putStrLn s
+print_ (Just v) = putStrLn (showValue v)
 
 repl :: IO ()
 repl = do
