0695da54f149336fb4720b642256ef0a862c247a
conao3 <conao3@gmail.com>
2025-06-10 05:39:18 +0900

    implement first repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
 module Main where
 
+import System.IO
+
+read_ :: IO String
+read_ = do
+    putStr "> "
+    hFlush stdout
+    getLine
+
+eval_ :: String -> String
+eval_ = id
+
+print_ :: String -> IO ()
+print_ = putStrLn
+
+repl :: IO ()
+repl = do
+    readed <- read_
+    print_ (eval_ readed)
+    repl
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = repl
